d87d015d383711cc6bbf92a5fa9a68f75328bfe5
apg258 <apg258@my.open.ac.uk>
2019-01-31 15:52:03 +0000

    2019-01-31 update

---
+++ 742-to-lower-case/to-lower-case.go
@@ -0,0 +1,36 @@
+// Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+//
+//  
+//
+//
+// Example 1:
+//
+//
+// Input: "Hello"
+// Output: "hello"
+//
+//
+//
+// Example 2:
+//
+//
+// Input: "here"
+// Output: "here"
+//
+//
+//
+// Example 3:
+//
+//
+// Input: "LOVELY"
+// Output: "lovely"
+//
+//
+//
+//
+//
+
+
+func toLowerCase(str string) string {
+    return strings.ToLower(str)
+}
